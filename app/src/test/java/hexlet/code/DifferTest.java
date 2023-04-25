4e463df3345a4dec4cef46e239175d3de1bf05fc
a.martynov <a.martynov@qiwi.com>
2023-04-25 08:53:11 +0300

    [step_6] Continuous integration. Add differ tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,19 +2,32 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DifferTest {
     @Test
-    void testShouldBeSuccessful() {
-        assertThat("11")
-                .isEqualTo("11");
+    void testShouldBeSuccessful() throws Exception {
+        var map1 = Map.of("key_1","value_1","key_2","value_2","key_3","value_3");
+        var map2 = Map.of("key_2","value_2","key_3","value_33","key_4","value_4");
+        var result = Differ.generate(map1,map2);
+        assertThat(result)
+                .isEqualTo("{\n" +
+                        "  - key_1: value_1 \n" +
+                        "  + key_2: value_2 \n" +
+                        "  - key_3: value_3 \n" +
+                        "  + key_3: value_33 \n" +
+                        "  + key_4: value_4 \n" +
+                        "}\n");
     }
 
     @Test
-    void testShouldBeFailed() {
-        assertThat("33")
-                .isEqualTo("33");
+    void testWithEmptyMaps() throws Exception {
+        var result = Differ.generate(Map.of(),Map.of());
+        assertThat(result)
+                .isEqualTo("{\n" +
+                        "}\n");
     }
 
 }
