4f875db01926408a2b59821fbfac515c7b4e2446
a.martynov <a.martynov@qiwi.com>
2023-04-25 09:12:40 +0300

    [step_6] Continuous integration. Fix codestyle

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -9,25 +9,25 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class DifferTest {
     @Test
     void testShouldBeSuccessful() throws Exception {
-        var map1 = Map.of("key_1","value_1","key_2","value_2","key_3","value_3");
-        var map2 = Map.of("key_2","value_2","key_3","value_33","key_4","value_4");
-        var result = Differ.generate(map1,map2);
+        var map1 = Map.of("key_1", "value_1", "key_2", "value_2", "key_3", "value_3");
+        var map2 = Map.of("key_2", "value_2", "key_3", "value_33", "key_4", "value_4");
+        var result = Differ.generate(map1, map2);
         assertThat(result)
-                .isEqualTo("{\n" +
-                        "  - key_1: value_1 \n" +
-                        "  + key_2: value_2 \n" +
-                        "  - key_3: value_3 \n" +
-                        "  + key_3: value_33 \n" +
-                        "  + key_4: value_4 \n" +
-                        "}\n");
+                .isEqualTo("{\n"
+                        + "  - key_1: value_1 \n"
+                        + "  + key_2: value_2 \n"
+                        + "  - key_3: value_3 \n"
+                        + "  + key_3: value_33 \n"
+                        + "  + key_4: value_4 \n"
+                        + "}\n");
     }
 
     @Test
     void testWithEmptyMaps() throws Exception {
-        var result = Differ.generate(Map.of(),Map.of());
+        var result = Differ.generate(Map.of(), Map.of());
         assertThat(result)
-                .isEqualTo("{\n" +
-                        "}\n");
+                .isEqualTo("{\n"
+                        + "}\n");
     }
 
 }
